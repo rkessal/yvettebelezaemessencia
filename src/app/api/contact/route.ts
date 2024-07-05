72efa90325ba7783c17aa825b115ba6b1a1f2ecc
rkessal <kessal.rayhan25@gmail.com>
2024-07-05 12:38:23 +0100

    why am I using vercel

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server'
 
 const config = {
   transporter: {
-    port: process.env.NEXT_PUBLIC_NODEMAILER_PORT as unknown as number,
-    host: process.env.NEXT_PUBLIC_NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT as unknown as number,
+    host: process.env.NODEMAILER_HOST,
     auth: {
-      user: process.env.NEXT_PUBLIC_NODEMAILER_USER,
-      pass: process.env.NEXT_PUBLIC_NODEMAILER_PASS,
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
     },
     secure: true,
   },
-  target: process.env.NEXT_PUBLIC_NODEMAILER_TARGET
+  target: process.env.NODEMAILER_TARGET
 };
 
 const transporter = nodemailer.createTransport(config.transporter);
